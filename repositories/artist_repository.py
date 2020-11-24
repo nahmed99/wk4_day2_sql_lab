01c947bddd5b748512487b58b3b45eb886aa5b3a
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-24 18:40:53 +0000

    select_all and delete functions written

--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -24,3 +24,27 @@ def select_all():
         artist = Artist(row['name'], row['id'] )
         artists.append(artist)
     return artists 
+
+
+def select(id):
+    artist = None
+    sql = "SELECT * FROM artists WHERE id = %s"  
+    values = [id] 
+    result = run_sql(sql, values)[0]
+    
+    # Check if the artist exists
+    if result is not None:
+        artist = Artist(result['name'], result['id'] )
+    return artist
+
+
+def delete_all():
+    sql = "DELETE  FROM artists" 
+    run_sql(sql)
+
+
+# DELETE - delete all
+def delete(id):
+    sql = "DELETE  FROM artists WHERE id = %s" 
+    values = [id]
+    run_sql(sql, values)
